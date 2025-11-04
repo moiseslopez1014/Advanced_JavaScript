7a8e54f537c3cd3d8cb1ffd9c7d7356fc9300f52
moiseslopez <moiseslopeziglesias@gmail.com>
2025-11-04 13:16:08 +0100

    terminao25

--- main.js
+++ main.js
@@ -176,3 +176,122 @@ console.log('\n===14. HasOwnProperty===');
 
 console.log(gato.hasOwnProperty('maullar'));
 console.log(gato.hasOwnProperty('sonido'));
+
+
+// 15 set
+console.log('\n=== 15 Set ===');
+
+const set15 = new Set([1,2,2,3,4]);
+
+set15.delete(3);
+
+console.log(set15.has(4));
+
+//16 anade a un set
+console.log('\n=== 16 Set add ===');
+
+set15.add(5)
+const [...set15toarray] = set15
+
+console.log(set15toarray);
+
+// 17 WeakSet 
+console.log('\n=== 17 WeakSet ===');
+
+const weak17 = new WeakSet();
+
+weak17.add({nombre: 'a'});
+weak17.add({nombre: 'b'});
+
+// 18 Map
+console.log('\n=== 18 MAP ===');
+
+
+const map18 = new Map();
+
+map18.set('gato', 'mamifero');
+map18.set('humano', 'mamifero');
+map18.set('serpiente', 'anfibio');
+map18.set('hormiga', 'insecto');
+map18.set('larva', 'insecto');
+
+for (const [k, v] of map18) {
+    console.log(`${k} => ${v}`);
+}
+console.log('numero objetos map: ',map18.size);
+
+//19 MAP con objetos en keys
+console.log('\n=== 19 MAP con objetos en claves ===');
+
+const map19 = new Map();
+
+map19.set({id:1}, 'objeto1')
+map19.set({id:2}, 'objeto2')
+map19.set({id:3}, 'objeto3')
+map19.set({id:4}, 'objeto4')
+map19.set({id:5}, 'objeto5')
+map19.set({id:6}, 'objeto6')
+
+console.log(map19);
+
+//20 WeakMap
+console.log('\n === 20 WeakMap ===');
+
+const usuarios20 = {
+    'nombre': 'Juan',
+}
+
+const weak20 = new WeakMap();
+
+weak20.set(usuarios20, 'abc123')
+console.log(weak20);
+
+// 21 Spread
+console.log('\n === 21 Spread ===');
+
+const arr21uno = [1,2];
+const arr21dos = [3,4];
+const arr21tre = [5,6];
+
+const arrSpreaded = [...arr21uno,...arr21dos,...arr21tre, 7,8]
+
+console.log(arr21uno,arr21dos,arr21tre,arrSpreaded);
+
+// 22 Rest
+console.log('\n=== 22 REST ===');
+
+const arr22unido = [10,20,30,40,50];
+
+const [primero22, segundo22, ...resto22] = arr22unido;
+
+console.log(primero22,segundo22,resto22);
+
+
+// 23 FLAT
+console.log('\n=== 23 FLAT ===');
+
+const arr23 = [1, [2, [3, [4]]]];
+
+console.log(arr23.flat(3));
+
+// 24 FLATMAP
+console.log('\n=== 24 FLATMAP ===');
+
+const arr24 = [2,4,6];
+
+console.log(arr24.flatMap(n => [n,n*0.5]));
+
+//25 MAP FOR OF
+console.log('\n=== 25 MAP FOR OF ===');
+
+const map25 = new Map();
+
+map25.set('clave1', 'valor1')
+map25.set('clave2', 'valor2')
+map25.set('clave3', 'valor3')
+map25.set('clave4', 'valor4')
+map25.set('clave5', 'valor5')
+
+for ([k,v] of map25) {
+    console.log(k,v);
+}
